77c46d584828036afb13c1105cfdcaa230781d0c
LukaPrcic12345 <bobsagott17@gmail.com>
2022-06-30 18:24:51 +0200

    image shows up

--- meme-generator/src/Components/Meme.js
+++ meme-generator/src/Components/Meme.js
@@ -3,12 +3,22 @@ import memesData from "../memesData";
 
 export default function Meme(props) {
 
+    const [memeImage,setImageUrl] = React.useState(" ")
+
     function getImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url;
-        console.log(url)
+
+        //TODO:Ne treba mi prethodna vrednost url koji je u ovom slucaju prazan string tkd ne radim ovako
+        // setImageUrl(memeImage => memeImage = url)
+
+        //TODO:Ovako odredjuem vrednost url
+        setImageUrl( url)
+
+        console.log(memeImage)
     }
+
 
     return (
         <main>
@@ -30,6 +40,7 @@ export default function Meme(props) {
                     Get a new meme image 🖼
                 </button>
             </div>
+            <img src={memeImage} className={'form--image'}/>
         </main>
     )
 }
